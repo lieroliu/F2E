fa60556c3cf9c22fb810d029856c47ec2a0db4a8
eatgether <lieroliu@gmail.com>
2021-10-26 15:38:33 +0800

    feat: create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import dog from './img/dog.jpeg';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				春日部防衛隊
+				<img src={dog} />
+			</header>
+		</div>
+	);
+}
+
+export default App;
